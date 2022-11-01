10d990b872fda5dc56e963d042eb4b0db447d1f5
mhtarora39 <mhtarora39@gmail.com>
2022-11-01 23:13:10 +0530

    Trapped Water Problem

---
+++ ProblemSolving/array/Rains.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int trap(vector<int> &height)
+{
+
+    const int size = height.size();
+
+    vector<int> left_max_height(size), right_max_height(size);
+
+    left_max_height[0] = height[0];
+    right_max_height[size - 1] = height[size - 1];
+    for (int i = 1; i < size; i++)
+    {
+        left_max_height[i] = max(left_max_height[i - 1], height[i]);
+        right_max_height[size - i - 1] = max(right_max_height[size - i], height[size - i - 1]);
+    }
+
+    int total_water = 0;
+    for (int i = 0; i < size; i++)
+    {
+        total_water += min(left_max_height[i], right_max_height[i]) - height[i];
+    }
+    return total_water;
+}
+
+int main() {
+    vector<int> res{4,2,0,3,2,5};
+    std::cout << " Total Water " << trap(res) << std::endl;
+}
--- ProblemSolving/array/Readme.md
+++ ProblemSolving/array/Readme.md
@@ -13,6 +13,11 @@
 
     Given an integer array arr, return the length of the longest subarray, which is a    mountain. Return 0 if there is no mountain subarray.
 * [Longest Consecutive Sequence](LongConsSeq.cpp) : Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.You must write an algorithm that runs in O(n) time.
+* [Rains](Rains.cpp) : Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+- ![alt text](Docs/Rains.png)
+
+
 
 
 
