a99bd036b44f2451eb194a578d3118f49a60a553
mhtarora39 <mhtarora39@gmail.com>
2022-12-15 10:09:50 +0530

    Working Replace

--- ProblemSolving/str/replace20.cpp
+++ ProblemSolving/str/replace20.cpp
@@ -1,38 +1,49 @@
 #include <iostream>
 
+std::string replace(std::string src, std::string from, std::string to)
+{
 
-void replace(std::string src, std::string from , std::string to) {
+    // Hi My Name is Mohit  -->  Calculate new space
+    int found = 0;
 
-//Hi My Name is Mohit  -->  Calculate new space
-int found = 0;
-
-// Calculate How many sub string are there
-for(int i = 0; i < src.size(); i++) {
-    if(src.substr(i, from.size()) == from) {
-       found++;
-    }
-}
-
-int new_len   = src.size() + found*(abs(to.size() - from.size())) + 1;
-
-int new_index = new_index -1;
-
-for(int i = src.size() -1 ; i >=0; i--) {
-    if(src.substr(i, from.size()) == from) {
-        for(int j = to.size()-1; j >= 0 ; j--) {
-            src[i-j] = to[j];
+    // Calculate How many sub string are there
+    for (int i = 0; i < src.size(); i++)
+    {
+        if (src.substr(i, from.size()) == from)
+        {
+            found++;
         }
-        new_index = new_index - to.size();
     }
-    else {
-       src[new_index] = src[i];
-       new_index--; 
+
+    int prev_len = src.size();
+
+    int new_len = src.size() + found * (abs(to.size() - from.size()));
+    src.resize(new_len);
+    int new_index = new_len - 1;
+    for (int i = prev_len - 1; i >= 0; i--)
+    {
+        //Search for substring.
+        if (src.substr(i, from.size()) == from)
+        {
+            for (int j = to.size() - 1; j >= 0; j--)
+            {
+                src[new_index - j] = to[to.size() - 1 - j];
+            }
+            new_index = new_index - to.size();
+        }
+        else
+        {
+            //else copy the string to new index.
+            src[new_index] = src[i];
+            new_index--;
+        }
     }
+    return src;
 }
 
-}
+int main()
+{
+    std::string newString = "My Name is Mohit";
 
-
-int main() {
-    return 1;
+    std::cout << " In String '" << newString << "' Replaced String: " << replace(newString," ","%20") << std::endl;
 }
