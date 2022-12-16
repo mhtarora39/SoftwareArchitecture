20c40d7795045336bec73857c792081800a50583
mhtarora39 <mhtarora39@gmail.com>
2022-12-16 12:57:38 +0530

    Generate Sequence Code

--- ProblemSolving/str/Readme.md
+++ ProblemSolving/str/Readme.md
@@ -1,2 +1,6 @@
 # String Solved Problems:
 * [Replace space with %20](replace20.cpp) : Write a method to replace all the spaces in a string with ‘%20’. You may assume that the string has sufficient space at the end to hold the additional characters and that you are given the “true” length of the string.
+* [Generate All the subsequence] (Subsequence.cpp) : Generate all the sub sequence of the given string. i.e 
+
+given string: abcd
+subsequence : a,b,c,d,ab,ac,ad,bc,bd,cd,abc,acd,abd,bcd,abcd
---
+++ ProblemSolving/str/Subsequence.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+void generateSubsequence(std::string in_str, std::string out_str, std::vector<std::string>& result) {
+    if(in_str.size() == 0) {
+        result.push_back(out_str);
+        return;
+    }
+    char ch = in_str[0];
+    generateSubsequence(in_str.substr(1), out_str + ch,result);
+    generateSubsequence(in_str.substr(1), out_str,result);
+
+}
+
+
+int main() {
+    std::vector<std::string> results;
+    generateSubsequence("abcd","",results);
+    for(auto& res : results) {
+        std::cout << res << ",";
+    }
+    
+}
