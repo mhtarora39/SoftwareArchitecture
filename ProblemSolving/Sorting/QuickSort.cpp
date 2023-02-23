e98ef55e3d48f8955e1078e42c472e04acedf7d8
mhtarora39 <mhtarora39@gmail.com>
2023-02-23 08:52:26 +0530

    Quick Sort Implementation

--- ProblemSolving/Sorting/QuickSort.cpp
+++ ProblemSolving/Sorting/QuickSort.cpp
@@ -29,17 +29,28 @@ int partition_a(std::vector<int>& array,int start_idx, int end_idx) {
 
 }
 
+int partition_clrs(std::vector<int>& array,int start_idx, int end_idx) {
+    int i = start_idx-1;
+    int pivot = array[end_idx];
+    for(int j = start_idx; j < end_idx; j++) {
+        if(array[j] < pivot) {
+            i++;
+            std::swap(array[i],array[j]);
+        }
+    }
+    std::swap(array[i+1] , array[end_idx]);
+    return i+1;
+}
+
 void quick_sort(std::vector<int>& array,int start_idx, int end_idx) 
 {
     if(start_idx >= end_idx) {
         return;
     }
 
-    int part_idx = partition_a(array,start_idx,end_idx);
+    int part_idx = partition_clrs(array,start_idx,end_idx);
     quick_sort(array,start_idx,part_idx-1);
     quick_sort(array,part_idx+1,end_idx); 
-
-    
 
 }
 
@@ -54,5 +65,6 @@ int main() {
     std::cout << "\n Sorted Array : \n"; 
     for(int i=0;i < arr.size(); i++) {
         std::cout<< arr[i] << " ";
-    }       
+    }
+    std::cout<< "\n";
 }
