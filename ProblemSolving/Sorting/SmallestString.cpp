7c0005a136c04d5d5c690f97be2d62c26ba0c6c3
mhtarora39 <mhtarora39@gmail.com>
2023-02-24 19:59:22 +0530

    Working Smallest String

--- ProblemSolving/Sorting/SmallestString.cpp
+++ ProblemSolving/Sorting/SmallestString.cpp
@@ -7,45 +7,57 @@ void merge_string(std::vector<std::string> &array, int start, int part, int end)
     int left = start;
     int right = part + 1;
     std::vector<std::string> tmp_array;
-    while (left < right && right <= end)
+    while (left <= part && right <= end)
     {
-        if (array[left] + array[left + 1] < array[right] + array[left])
+        if (array[left] + array[right] < array[right] + array[left])
+        {
+            tmp_array.push_back(array[left]);
+            left++;
+        }
+        else
         {
             tmp_array.push_back(array[right]);
             right++;
         }
-        else
-        {
-            left++;
-            tmp_array.push_back(array[right]);
-        }
     }
 
+    while (left <= part)
+    {
+        tmp_array.push_back(array[left++]);
+    }
+
+    while (right <= end)
+    {
+        tmp_array.push_back(array[right++]);
+    }
+    for(int i = 0; i < tmp_array.size(); i++) {
+        array[i + start] = tmp_array[i];
+    }
 
 }
 
-std::string part_strings(std::vector<std::string> &array, int start, int end)
+void part_strings(std::vector<std::string> &array, int start, int end)
 {
 
     if (start >= end)
     {
-        return "";
+        return ;
     }
     int mid = (start + end) / 2;
 
     part_strings(array, start, mid);
     part_strings(array, mid + 1, end);
     merge_string(array, start, mid, end);
-    std::string tmp = "";
-    for(auto str : array) {
-        tmp += str;
-    }
-    return tmp;
 }
+    
 
 int main()
 {
     std::vector<std::string> array = {"a", "ab", "aaa"};
-    std::cout << " answer should be 'aaaaab'" << part_strings(array,0,array.size()-1) << std::endl;
-     
+    std::string tmp = "";
+    part_strings(array,0,array.size() - 1);
+    for(auto str: array) {
+        tmp += str;
+    }
+    std::cout << " answer should be 'aaaaab' but result is : " << tmp << std::endl;
 }
